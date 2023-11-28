a9bbdfbefb33f330addfe01da234bf749c6b9387
Matheus Almeida <matheusalmeida016@gmail.com>
2023-11-28 09:43:58 -0400

    feat: add padding

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import { TextField } from './text-field'
 
 export const App = () => {
   return (
-    <div className='min-h-full bg-gray-600 flex items-center justify-center'>
+    <div className='min-h-full bg-gray-600 flex items-center justify-center p-4'>
       <TextField />
     </div>
   )
